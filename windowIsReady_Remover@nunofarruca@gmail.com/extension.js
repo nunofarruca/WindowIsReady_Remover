96fa5bea55e7bac285b0c78780164ffc740a53bd
Nuno Farruca <nmf@localhost.localdomain>
2015-11-05 13:33:10 +0000

    First Commit, working in fedora 23, gnome 3.18

---
+++ windowIsReady_Remover@nunofarruca@gmail.com/extension.js
@@ -0,0 +1,34 @@
+const Main = imports.ui.main;
+const WindowAttentionHandler = imports.ui.windowAttentionHandler;
+const Shell = imports.gi.Shell;
+const Lang = imports.lang;
+
+function WindowIsReadyRemover() {
+    this._init();
+}
+
+WindowIsReadyRemover.prototype = {
+    
+    _init : function() {
+        this._tracker = Shell.WindowTracker.get_default();
+		log('Disabling Window Is Ready Notification')
+		global.display.disconnect(Main.windowAttentionHandler._windowDemandsAttentionId);
+    },
+
+    destroy: function () {
+        global.display.disconnect(this._handlerid);
+    }
+}
+
+let windowIsReadyRemover;
+
+function init() {
+}
+
+function enable() {
+    windowIsReadyRemover = new WindowIsReadyRemover();
+}
+
+function disable() {
+    windowIsReadyRemover.destroy();
+}
