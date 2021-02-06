3a97b3e86d0a47036a5774ed3160404cec1218f9
Nuno Farruca <nunofarruca@gmail.com>
2021-02-06 11:55:00 +0000

    Adding pref and preventing automatic disable calls

--- README.md
+++ README.md
@@ -3,7 +3,11 @@ Removes window is ready Notification
 
 This removes the annoying "Windows is Ready" notification.
 
-It disconnect the WindowAttentionHandler listenner, so you will never get that message.
-This was only possible after gnome 3.18.
+It disconnect the WindowAttentionHandler listener, so you will never get that message.
 
-Tested using Fedora 33 with gnome 3.38
+By default, disable() calls by the system are prevented.
+Can be changed on preferences.
+
+To fully disable extension please restart gnome-shell.
+
+(Tested using Fedora 33 with gnome 3.38)
--- windowIsReady_Remover@nunofarruca@gmail.com/extension.js
+++ windowIsReady_Remover@nunofarruca@gmail.com/extension.js
@@ -2,6 +2,22 @@
 
 const Main = imports.ui.main;
 const Handler = Main.windowAttentionHandler;
+const Gio = imports.gi.Gio;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+
+function getSettings() {
+    let GioSSS = Gio.SettingsSchemaSource;
+    let schemaSource = GioSSS.new_from_directory(
+        Me.dir.get_child("schemas").get_path(),
+        GioSSS.get_default(),
+        false
+    );
+    let schemaObj = schemaSource.lookup('org.gnome.shell.extensions.windowIsReady_Remover', true);
+    if (!schemaObj) {
+        throw new Error('cannot find schemas');
+    }
+    return new Gio.Settings({ settings_schema: schemaObj });
+}
 
 function init() {
 }
@@ -18,20 +34,27 @@ function enable() {
 }
 
 function disable() {
-    if (!Handler._windowDemandsAttentionId) {
-        Handler._windowDemandsAttentionId = global.display.connect(
-            'window-demands-attention',
-            (display, window) => {
-                Handler._onWindowDemandsAttention(display, window);
-            }
-        );
-    }
-    if (!Handler._windowMarkedUrgentId) {
-        Handler._windowMarkedUrgentId = global.display.connect(
-            'window-marked-urgent',
-            (display, window) => {
-                Handler._onWindowDemandsAttention(display, window);
-            }
-        );
+    let settings = getSettings();
+    let preventDisable = settings.get_boolean('prevent-disable');
+
+    if (preventDisable) {
+        //log(Me.metadata.name + ' > Disable was prevented');
+    } else {
+        if (!Handler._windowDemandsAttentionId) {
+            Handler._windowDemandsAttentionId = global.display.connect(
+                'window-demands-attention',
+                (display, window) => {
+                    Handler._onWindowDemandsAttention(display, window);
+                }
+            );
+        }
+        if (!Handler._windowMarkedUrgentId) {
+            Handler._windowMarkedUrgentId = global.display.connect(
+                'window-marked-urgent',
+                (display, window) => {
+                    Handler._onWindowDemandsAttention(display, window);
+                }
+            );
+        }
     }
 }
